b1ec996a5abed750df7602c1a86f62df5fb8e89e
Wuang Shen <issacshen2015@gmail.com>
2019-05-06 22:56:33 +1000

    implemented create profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,16 @@
 class ProfileController < ApplicationController
+	def register
+		profile = Profile.new(profile_params)
+		if profile.save
+			render json:{status:'SUCCESS', message:'Saved Profile', data: profile}, status: :ok
+		else
+			render json:{status:'ERROR', message:'Not Saved', data: profile.errors}, status: :unprocessable_entity
+		end
+
+		private
+
+		def profile_params
+			params.permit(:userName, :address, :birthDate, :email, :name, :password)
+		end
+	end
 end
